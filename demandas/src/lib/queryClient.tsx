73f4ae947747de1f1e9c892ad0e3507d5e6b158d
Felipe Alisson <felipe.alissonpsantos@gmail.com>
2025-08-15 15:00:11 -0300

    Delete demandas directory

--- demandas/src/lib/queryClient.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import * as React from "react";
-
-const client = new QueryClient();
-
-export default function QueryProvider({ children }: { children: React.ReactNode }) {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
-}
